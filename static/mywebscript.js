af81080a7eecab39bcbe4fcb378732ab5d78acee
Chen Xu <17456228+chennnxu@users.noreply.github.com>
2024-03-25 21:21:29 +0100

    ✅ add coments

--- static/mywebscript.js
+++ static/mywebscript.js
@@ -1,12 +1,19 @@
+// Function to run sentiment analysis when called
 let RunSentimentAnalysis = ()=>{
+    // Get the text to analyze from the input field with id "textToAnalyze"
     textToAnalyze = document.getElementById("textToAnalyze").value;
-
+    // Create a new XMLHttpRequest object to make a request to the server
     let xhttp = new XMLHttpRequest();
+    // Define a function to handle the response from the server
     xhttp.onreadystatechange = function() {
+        // Check if the request is complete and the response is ready
         if (this.readyState == 4 && this.status == 200) {
+            // Update the HTML content of an element with id "system_response" with the response text
             document.getElementById("system_response").innerHTML = xhttp.responseText;
         }
     };
+    // Open a GET request to the server with the specified endpoint and parameters
     xhttp.open("GET", "sentimentAnalysis?textToAnalyze"+"="+textToAnalyze, true);
+    // Send the request to the server
     xhttp.send();
 }
